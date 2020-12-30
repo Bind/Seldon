83ff5b78b261bd408be22ec2b568143220ca1719
bind <douglasjbinder@gmail.com>
2020-12-29 19:07:32 -0600

    try using indexes

--- src/routines/take.js
+++ src/routines/take.js
@@ -3,7 +3,7 @@ import {
   planetPercentEnergy,
   modelEnergyNeededToTake,
 } from "../utils/planet.js";
-import { createDelayedMove, createChainedMove } from "../subroutines.js";
+import { createDelayedMove, createChainedMove } from "../subroutines";
 
 import { secondsToMs, msToSeconds } from "../utils/time.js";
 
